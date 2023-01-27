1bf24ce4b862e86ac7cd05998da866dedbcffd11
Anna <101894235+annFromEarth@users.noreply.github.com>
2023-01-27 20:32:43 +0300

    comments

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ module.exports = function check(str, bracketsConfig) {
   
   else {
 
-    const Brackets = bracketsConfig.map(x=> x.join().replace(",", "")) ;  /**join bracket pairs in the configs to look for the "central pair" **/
+    const Brackets = bracketsConfig.map(x=> x.join().replace(",", "")) ;   /*join bracket pairs in the configs to look for the "central pair" */
 
-    let strNew     /**prepare for the search of matches between the config and the string */
+    let strNew                                                             /*prepare for the search of matches between the config and the string */
     let i=0;
 
     function testStringOnce(x) {
 
         strNew = x.replace(Brackets[i], "");
         i++;
-        if (i==Brackets.length) {return strNew;}   /* all the brackets config checked once*/
-        else return testStringOnce(strNew);        /* if not -- continue*/
+        if (i==Brackets.length) {return strNew;}                           /*all the brackets config checked once*/
+        else return testStringOnce(strNew);                                /*if not -- continue*/
     }
 
-     if (testStringOnce(str).length == str.length) {return false;}  /*string not empty and nothing more to replace*/
-     else return check(strNew, bracketsConfig);    /* start from the beginning - as this time another brackets config might be in the center*/
+     if (testStringOnce(str).length == str.length) {return false;}         /*string not empty and nothing more to replace*/
+     else return check(strNew, bracketsConfig);                            /*start again - this time another brackets config might be in the center*/
 } }

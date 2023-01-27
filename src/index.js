ecfe655c39ceae6d8a7704155d4f830de581f67f
annFromEarth <dv_anna@mail.ru>
2023-01-27 20:02:44 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const Brackets = bracketsConfig.map(x=> x.join().replace(",", "")) ;
+
+  
+if (str.length==0) {return true;}
+
+else {
+  let strNew 
+  let i=0;
+
+  function testStringOnce(x) {
+
+      strNew = x.replace(Brackets[i], "");
+      i++;
+      if (i==Brackets.length) {return strNew;}   /* all the brackets config checked once*/
+      else return testStringOnce(strNew);        /* if not -- continue*/
+
+  }
+
+   if (testStringOnce(str).length == str.length) {return false;}  /*string not empty and nothing more to replace*/
+   else return check(strNew, bracketsConfig);    /* start from the beginning - as this time another brackets config might be in the center*/
+
+} }

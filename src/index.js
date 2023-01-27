7d8cf2456b669f45f733a8a20329fed99ee91b40
annFromEarth <dv_anna@mail.ru>
2023-01-27 20:18:46 +0300

    done_

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
 
-  const Brackets = bracketsConfig.map(x=> x.join().replace(",", "")) ;
-
+  if (str.length==0) {return true;}
   
-if (str.length==0) {return true;}
+  else {
 
-else {
-  let strNew 
-  let i=0;
+    const Brackets = bracketsConfig.map(x=> x.join().replace(",", "")) ;  /**join bracket pairs in the configs to look for the "central pair" **/
 
-  function testStringOnce(x) {
+    let strNew     /**prepare for the search of matches between the config and the string */
+    let i=0;
 
-      strNew = x.replace(Brackets[i], "");
-      i++;
-      if (i==Brackets.length) {return strNew;}   /* all the brackets config checked once*/
-      else return testStringOnce(strNew);        /* if not -- continue*/
+    function testStringOnce(x) {
 
-  }
+        strNew = x.replace(Brackets[i], "");
+        i++;
+        if (i==Brackets.length) {return strNew;}   /* all the brackets config checked once*/
+        else return testStringOnce(strNew);        /* if not -- continue*/
+    }
 
-   if (testStringOnce(str).length == str.length) {return false;}  /*string not empty and nothing more to replace*/
-   else return check(strNew, bracketsConfig);    /* start from the beginning - as this time another brackets config might be in the center*/
-
+     if (testStringOnce(str).length == str.length) {return false;}  /*string not empty and nothing more to replace*/
+     else return check(strNew, bracketsConfig);    /* start from the beginning - as this time another brackets config might be in the center*/
 } }
